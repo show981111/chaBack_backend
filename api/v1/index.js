41520766efb618d4bfe3956181c89734ee3304fc
show981111 <show981111@gmail.com>
2021-01-24 00:03:56 +0900

    versioning api using router

---
+++ api/v1/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const router = express()
+
+var communityRouter = require('./routes/communities.routes.js');
+var commentRouter = require('./routes/comments.routes.js');
+var userRouter = require('./routes/user.routes.js');
+var authRouter = require('./routes/auth.routes.js');
+var placeRouter = require('./routes/place.routes.js');
+var reviewRouter = require('./routes/review.routes.js');
+var replyRouter = require('./routes/reply.routes.js');
+var resourcesRouter = require('./routes/resources.routes.js');
+var likeRouter = require('./routes/like.routes.js');
+var wishRouter = require('./routes/wish.routes.js');
+
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerOptions = {
+	swaggerDefinition: {
+		info : {
+			title: 'chaBack',
+			version: '1.0.0',
+			description: 'Api description for chaBack',
+		},
+        basePath: "/api/v1",
+	},
+	apis : ["./api/v1/docs/*.yaml"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+router.use('/community', communityRouter);
+router.use('/comment', commentRouter);
+router.use('/user', userRouter);
+router.use('/auth', authRouter);
+router.use('/place', placeRouter);
+router.use('/review', reviewRouter);
+router.use('/reply', replyRouter);
+router.use('/resources', resourcesRouter);
+router.use('/like', likeRouter);
+router.use('/wish', wishRouter);
+
+module.exports = router;
+
+
+
+
+
--- app.js
+++ app.js
@@ -3,49 +3,17 @@ require('dotenv').config();
 const app = express()
 const port = process.env.port;
 // const mongoDB = require('./dbConnection/mongoDB.js')
-const mariaDB = require('./dbConnection/mariaDB.js')
-var communityRouter = require('./api/v1/routes/communities.routes.js');
-var commentRouter = require('./api/v1/routes/comments.routes.js');
-var userRouter = require('./api/v1/routes/user.routes.js');
-var authRouter = require('./api/v1/routes/auth.routes.js');
-var placeRouter = require('./api/v1/routes/place.routes.js');
-var reviewRouter = require('./api/v1/routes/review.routes.js');
-var replyRouter = require('./api/v1/routes/reply.routes.js');
-var resourcesRouter = require('./api/v1/routes/resources.routes.js');
-var likeRouter = require('./api/v1/routes/like.routes.js');
-var wishRouter = require('./api/v1/routes/wish.routes.js');
+const version_one_router = require('./api/v1/index.js');
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerOptions = {
-	swaggerDefinition: {
-		info : {
-			title: 'chaBack',
-			version: '1.0.0',
-			description: 'Api description for chaBack',
-		},
-        basePath: "/api/v1",
-	},
-	apis : ["./api/v1/docs/*.yaml"]
-};
-
+//body parser 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 // mongoDB();
 
-const swaggerDocs = swaggerJsDoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-app.use('/api/v1/community', communityRouter);
-app.use('/api/v1/comment', commentRouter);
-app.use('/api/v1/user', userRouter);
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/place', placeRouter);
-app.use('/api/v1/review', reviewRouter);
-app.use('/api/v1/reply', replyRouter);
-app.use('/api/v1/resources', resourcesRouter);
-app.use('/api/v1/like', likeRouter);
-app.use('/api/v1/wish', wishRouter);
+//versioning
+app.use('/api/v1/', version_one_router);
+
 
 //error handler
 app.use((err, req, res, next) => {
